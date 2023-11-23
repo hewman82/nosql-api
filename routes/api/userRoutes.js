9338725f97726f6965b0060fa16316850cfb4b0d
hewman82 <138827425+hewman82@users.noreply.github.com>
2023-11-23 15:57:23 -0800

    routes pseudocode

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,35 @@
+// Get route for all users
+// '/api/users'
+// app.GET('/')
+
+// Post route to add user
+// '/api/users'
+// app.POST('/')
+
+// Get route for single user
+// '/api/users/:id'
+// app.GET(':id')
+
+// Put route to update user
+// '/api/users/:id'
+// app.PUT(':id')
+
+// Delete route to delete user
+// '/api/users/:id'
+// app.DELETE('/:id')
+
+// Get route to get list of user's friends
+// '/api/users/:id/friends'
+// app.GET('/:id/friends')
+
+// Get route to get one of user's friends
+// '/api/users/:id/friends/:id'
+// app.GET('/:id/friends/:id')
+
+// Post route to add a user to friend list
+// '/api/users/:id/friends'
+// app.POST('/:id/friends')
+
+// Delete route to remove a user from friend list
+// '/api/users/:id/friends/:id'
+// app.DELETE('/:id/friends/:id')
