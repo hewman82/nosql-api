87e886b5375dccd94e6d623cb262dc7323f08a9d
hewman82 <138827425+hewman82@users.noreply.github.com>
2023-11-23 18:17:45 -0800

    reactionSchema

---
+++ models/Reaction.js
@@ -0,0 +1,34 @@
+const { ObjectId } = require('bson');
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: formatDate
+    }
+
+  }
+);
+
+function formatDate(createdAt) {
+    const formatMe = new Date(createdAt);
+    const local = formatMe.toLocaleDateString();
+    return local;
+};
+
+module.exports = reactionSchema;
